ff15deacf5c1b1bfc4e07ac53611df2a0aea8f2d
Frankly <frankly.666@qq.com>
2024-07-13 22:37:41 +0800

    ignore key file

--- src/router/login.router.js
+++ src/router/login.router.js
@@ -2,13 +2,11 @@ const koaRouter = require("@koa/router");
 const { verifyLogin, verifyAuth } = require("../middleware/login.middleware");
 const { login, test } = require("../controller/loginController");
 
-
 const loginRouter = new koaRouter({
-  prefix: "/login"
-})
+  prefix: "/login",
+});
 
 loginRouter.post("/", verifyLogin, login);
-loginRouter.post("/test",verifyAuth, test)
+loginRouter.post("/test", verifyAuth, test);
 
-module.exports =  loginRouter
-
+module.exports = loginRouter;
