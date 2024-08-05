e9aceb7902b6653ddb631eedfe24c230e2c43b70
Frankly <xushuai@baicizhan.com>
2024-08-05 18:09:20 +0800

    feat: 增添接口, 使用token来返回用户信息

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -3,6 +3,7 @@ const { PRIVATE_KEY } = require("../config/key");
 const { TOKEN_IS_WRONG } = require("../config/error");
 const { findUserRole } = require("../dbService/user.service");
 const { mapPermission } = require("../utils/mapPermission");
+const { SERVE_PORT, SERVER_HOST } = require("../config/serve");
 
 class loginController {
   async login(ctx, next) {
@@ -12,23 +13,39 @@ class loginController {
     // 颁发证书
     try {
       const token = jwt.sign({ id, name }, PRIVATE_KEY, {
-        expiresIn: '7d', // 过期时间，例如7天
-        algorithm: 'RS256' // 使用的算法
+        expiresIn: "7d", // 过期时间，例如7天
+        algorithm: "RS256", // 使用的算法
       });
 
       // 还需要去查询用户的权限信息, 并进行信息处理
-      const  res = await findUserRole(id);
-      const permission = mapPermission(res[0].role.permission)
+      const res = await findUserRole(id);
+      const permission = mapPermission(res[0].role.permission);
       res[0].role.permission = permission;
 
-      ctx.body = {code: 0, data:{token, user: res[0]}};
+      const avatar = `${SERVER_HOST + ":" + SERVE_PORT + "/file/avatar/" + id}`;
+
+      ctx.body = { code: 0, data: { token, avatar: avatar, user: res[0] } };
     } catch (error) {
-      return ctx.app.emit("error", TOKEN_IS_WRONG, ctx)
+      return ctx.app.emit("error", TOKEN_IS_WRONG, ctx);
     }
   }
 
-  test(ctx, next) {
-    ctx.body = "你有权限访问!"
+  async flushedData(ctx, next) {
+    // 获取用户信息
+    const { id, name } = ctx.user;
+
+    try {
+      // 查询用户的权限信息, 并进行信息处理
+      const res = await findUserRole(id);
+      const permission = mapPermission(res[0].role.permission);
+      res[0].role.permission = permission;
+
+      const avatar = `${SERVER_HOST + ":" + SERVE_PORT + "/file/avatar/" + id}`;
+
+      ctx.body = { code: 0, data: { avatar: avatar, user: res[0] } };
+    } catch (error) {
+      return ctx.app.emit("error", TOKEN_IS_WRONG, ctx);
+    }
   }
 }
 
--- src/router/login.router.js
+++ src/router/login.router.js
@@ -1,12 +1,12 @@
 const koaRouter = require("@koa/router");
 const { verifyLogin, verifyAuth } = require("../middleware/login.middleware");
-const { login, test } = require("../controller/loginController");
+const { login, flushedData } = require("../controller/loginController");
 
 const loginRouter = new koaRouter({
   prefix: "/login",
 });
 
 loginRouter.post("/", verifyLogin, login);
-loginRouter.post("/test", verifyAuth, test);
+loginRouter.post("/flush", verifyAuth, flushedData);
 
 module.exports = loginRouter;
